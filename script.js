ef23b0a04e491fb09981f14f1d7e7afd925db3a0
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-10 19:04:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Example(I) changed.";
+}
+
+function button() {
+  document.getElementById("picture-of-my-favirot-game").src="./Images/Barry Bonson .jpeg"
+}
+
+function  button2() {
+  document.getElementById("background").style.backgroundColor ="gray"
+}
+
+function button3() {
+  document.getElementById("ExampleI").style.display = "none";
+}
